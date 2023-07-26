56e2a6ee063fdc2978abdc2202a0f55d8955b293
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-07-26 08:14:57 +0200

    Add async logic

---
+++ AsyncExecute.cs
@@ -0,0 +1,81 @@
+﻿
+
+using System.Diagnostics;
+using System.Net;
+
+internal static class AsyncExecute
+{
+    public static async Task ExecuteAsync()
+    {
+        var watch = Stopwatch.StartNew();
+
+        await RunDownloadParallelAsync();
+
+        watch.Stop();
+
+        var elapsedMs = watch.ElapsedMilliseconds;
+
+        Console.WriteLine($"Total execution time: {elapsedMs}");
+    }
+
+    private static async Task RunDownloadParallelAsync()
+    {
+        List<string> websites = PrepData();
+
+        List<Task<WebsiteDataModel>> tasks = new List<Task<WebsiteDataModel>>();
+
+        foreach (string site in websites)
+        {
+            tasks.Add(Task.Run(() => DownloadWebsite(site)));
+        }
+
+        var results = await Task.WhenAll(tasks);
+
+        foreach(var item in results)
+        {
+            ReportWebsiteInfo(item);
+        }
+    }
+
+    private static async Task RunDownloadAsync()
+    {
+        List<string> websites = PrepData();
+
+        foreach (string site in websites)
+        {
+            WebsiteDataModel results = await Task.Run(() => DownloadWebsite(site));
+            ReportWebsiteInfo(results);
+        }
+    }
+
+    private static List<string> PrepData()
+    {
+        List<string> output = new List<string>();
+
+        Console.Clear();
+
+        output.Add("https://www.yahoo.com");
+        output.Add("https://www.google.com");
+        output.Add("https://www.codeproject.com");
+        output.Add("https://www.documentwarehouse.com.na");
+        //output.Add("https://www.mfiles.com");
+
+        return output;
+    }
+
+    private static WebsiteDataModel DownloadWebsite(string websiteURL)
+    {
+        WebsiteDataModel output = new WebsiteDataModel();
+        WebClient client = new();
+
+        output.WebsiteUrl = websiteURL;
+        output.WebsiteData = client.DownloadString(websiteURL);
+
+        return output;
+    }
+
+    private static void ReportWebsiteInfo(WebsiteDataModel data)
+    {
+        Console.WriteLine($"{data.WebsiteUrl} downloaded: {data.WebsiteData.Length} characters long");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,10 @@
-﻿
-
-using System.Diagnostics;
-using System.Net;
-using System.Runtime.InteropServices;
-
-bool running = true;
+﻿bool running = true;
 
 Initialize();
 
 while (running)
 {
-    Console.Write(">");
+    Console.Write("> ");
     Int32.TryParse(Console.ReadLine(), out int input);
     ProcessInput(input);
 }
@@ -66,84 +60,4 @@ void PrintInvalidCommand()
     Console.WriteLine("Command not recognized, please try again.");
     Console.WriteLine("Type \"0\" for available commands.");
     Console.WriteLine();
-}
-
-Console.WriteLine("Starting...");
-Console.ReadKey(true);
-
-internal static class AsyncExecute
-{
-    public static async Task ExecuteAsync()
-    {
-        var watch = Stopwatch.StartNew();
-
-        await RunDownloadParallelAsync();
-
-        watch.Stop();
-
-        var elapsedMs = watch.ElapsedMilliseconds;
-
-        Console.WriteLine($"Total execution time: {elapsedMs}");
-    }
-
-    private static async Task RunDownloadParallelAsync()
-    {
-        List<string> websites = PrepData();
-
-        List<Task<WebsiteDataModel>> tasks = new List<Task<WebsiteDataModel>>();
-
-        foreach (string site in websites)
-        {
-            tasks.Add(Task.Run(() => DownloadWebsite(site)));
-        }
-
-        var results = await Task.WhenAll(tasks);
-
-        foreach(var item in results)
-        {
-            ReportWebsiteInfo(item);
-        }
-    }
-
-    private static async Task RunDownloadAsync()
-    {
-        List<string> websites = PrepData();
-
-        foreach (string site in websites)
-        {
-            WebsiteDataModel results = await Task.Run(() => DownloadWebsite(site));
-            ReportWebsiteInfo(results);
-        }
-    }
-
-    private static List<string> PrepData()
-    {
-        List<string> output = new List<string>();
-
-        Console.Clear();
-
-        output.Add("https://www.yahoo.com");
-        output.Add("https://www.google.com");
-        output.Add("https://www.codeproject.com");
-        output.Add("https://www.documentwarehouse.com.na");
-        //output.Add("https://www.mfiles.com");
-
-        return output;
-    }
-
-    private static WebsiteDataModel DownloadWebsite(string websiteURL)
-    {
-        WebsiteDataModel output = new WebsiteDataModel();
-        WebClient client = new();
-
-        output.WebsiteUrl = websiteURL;
-        output.WebsiteData = client.DownloadString(websiteURL);
-
-        return output;
-    }
-
-    private static void ReportWebsiteInfo(WebsiteDataModel data)
-    {
-        Console.WriteLine($"{data.WebsiteUrl} downloaded: {data.WebsiteData.Length} characters long");
-    }
 }
